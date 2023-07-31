48e304049b0a6fa4a43e939c61616100058229a2
Shinovon <arman200711@gmail.com>
2023-07-31 13:53:27 +0500

    One more fix

--- plugin/inc/PiglerServer.h
+++ plugin/inc/PiglerServer.h
@@ -26,6 +26,5 @@ public:
 	void ServiceError(const RMessage2& message, TInt error);
 private:
 	TPiglerMessage ReadMessage(const RMessage2& message);
-	TPiglerMessage ReadIconMessage(const RMessage2& message);
 };
 #endif
